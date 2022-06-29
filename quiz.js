f97a92d46adfe20d6e690855fe9f9b211036b5ea
MirzaAyon <ayonmirza98@gmail.com>
2022-06-29 22:59:44 +0600

    quiz added

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function work(x, y = 4) {
+    return x + y;
+}
+console.log(work(32);
+   //ans : syntax error
+
+   //quiz 10
+   Can you access the let variable outside of the block scope ?
+    //no
+
+    //    quiz - 11
+    JS is a _____ type programming language.
+//dynamic
+//primitive datatype in JS
+//BigInt
